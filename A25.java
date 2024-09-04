3973f0e1957c5449aaa1eff62b3c471bc4675408
yash0219 <yashtyagi219@gmail.com>
2024-09-04 08:43:19 +0530

    gud

---
+++ A25.java
@@ -0,0 +1,22 @@
+class Employee
+{
+    int eid;
+    String name;
+    String department;
+    void f1()
+    {
+   System.out.println("yo");
+    }
+    void f1(int eid)
+   {
+     System.out.println(eid);
+   }
+}
+class A25
+{
+  public static void main (String[] args)
+{
+
+
+}
+}
